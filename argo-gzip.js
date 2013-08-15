19ac1fbe7bec3c610a4a6961d05e93f5dfdc59a7
Matt Dobson <mdobson4@gmail.com>
2013-08-15 12:33:23 -0400

    updated module.

--- argo-gzip.js
+++ argo-gzip.js
@@ -1,1 +1,78 @@
-var zlib = require('zlib');
+      var enc_type = env.target.response.headers['content-encoding'];
+      var response_buffer = []
+      if (enc_type && enc_type === 'gzip') {
+        var unzip = zlib.createGunzip();
+        console.log("gzip");
+        env.target.response.pipe(unzip);
+        unzip.on('data', function(data){
+          console.log('data event');
+          response_buffer.push(data.toString());
+        }).on('end', function(){
+          var venues = [];
+          var body = JSON.parse(response_buffer.join(""));
+          console.log(body);
+          if(body.response.groups) {
+            body.response.groups.forEach(function(group){
+              group.items.forEach(function(venue){
+                venues.push({
+                  "name":venue.venue.name, 
+                  "city":venue.venue.location.city,
+                  "location":{
+                    "lat":venue.venue.location.lat, 
+                    "lng":venue.venue.location.lng
+                  }
+                });
+              });
+            });
+          } else {
+            venues.push({
+              "name":body.response.venue.name,
+              "city":body.response.venue.location.city,
+                  "location":{
+                    "lat":body.response.venue.location.lat, 
+                    "lng":body.response.venue.location.lng
+                  }
+            });
+          }
+          var response_body = JSON.stringify(venues);
+          var buf = new Buffer(response_body);
+          zlib.gzip(buf, function (_, result) {   
+            env.target.response.body = result; 
+            next(env);
+          });
+        }).on('error', function(error){
+          console.log(error);
+        })
+      } else {
+        env.target.response.getBody(function(error, body){
+          var venues = [];
+          var body = JSON.parse(body.toString('utf-8'));
+          if(body.response.groups) {
+            body.response.groups.forEach(function(group){
+              group.items.forEach(function(venue){
+                venues.push({
+                  "name":venue.venue.name, 
+                  "city":venue.venue.location.city,
+                  "location":{
+                    "lat":venue.venue.location.lat, 
+                    "lng":venue.venue.location.lng
+                  }
+                });
+              });
+            });
+          } else if(body.response.venue) {
+            venues.push({
+              "name":body.response.venue.name,
+              "city":body.response.venue.location.city,
+                  "location":{
+                    "lat":body.response.venue.location.lat, 
+                    "lng":body.response.venue.location.lng
+                  }
+            });
+          } else {
+            next(env);
+          }
+          env.target.response.body = JSON.stringify(venues); 
+          next(env);
+        });
+      }
--- index.js
+++ index.js
@@ -1,8 +1,58 @@
 var argo = require("argo"),
     url = require("url"),
-    zlib = require("zlib");
+    zlib = require("zlib"),
+    http = require("http");
+
+var zipPackage = function(argo) {
+  return {
+    name:"gzip compression",
+    install: function() {
+      var serverResponseProto = argo._http.ServerResponse.prototype;
+      var serverRequestProto = argo._http.IncomingMessage.prototype
+      if(serverRequestProto._argoModified) {
+        serverRequestProto._gzipModified = "request";
+        var getRequestBodyFunc = serverRequestProto.getBody;
+        serverRequestProto.getBody = function(cb) {
+          var self = this;
+          getRequestBodyFunc.call(this, function(error, body){
+            console.log(error);
+            if("accept-encoding" in self.headers) {
+              var encoding = self.headers["accept-encoding"];
+              if (encoding.indexOf('gzip') !== -1) {
+                var unzip = zlib.createGunzip();
+                var responseBuf = [] 
+                self.pipe(unzip);
+                unzip
+                  .on('data', function(data){
+                    responseBuf.push(data.toString());
+                  })
+                  .on('end', function(){
+                    var body = responseBuf.join("");
+                    cb(null, body);
+                  })
+                  .on('error', function(error){
+                    cb(error)
+                  });
+              } else {
+                cb(null, body);
+              }
+            } else {
+              cb(null, body);
+            }
+          });
+        }
+      }
+
+      if(serverResponseProto._argoModified) {
+        serverResponseProto._gzipModified = "response";
+      }
+      
+    }
+  }
+}
 
 argo()
+  .include({package:zipPackage})
   .use(function(handle) {
     //Lets transplant our credentials via our proxy
     handle("request", function(env, next){
@@ -26,84 +76,14 @@ argo()
      next(env);
     });
     handle("response", function(env, next){
-      var enc_type = env.target.response.headers['content-encoding'];
-      var response_buffer = []
-      if (enc_type && enc_type === 'gzip') {
-        var unzip = zlib.createGunzip();
-        console.log("gzip");
-        env.target.response.pipe(unzip);
-        unzip.on('data', function(data){
-          console.log('data event');
-          response_buffer.push(data.toString());
-        }).on('end', function(){
-          var venues = [];
-          var body = JSON.parse(response_buffer.join(""));
-          console.log(body);
-          if(body.response.groups) {
-            body.response.groups.forEach(function(group){
-              group.items.forEach(function(venue){
-                venues.push({
-                  "name":venue.venue.name, 
-                  "city":venue.venue.location.city,
-                  "location":{
-                    "lat":venue.venue.location.lat, 
-                    "lng":venue.venue.location.lng
-                  }
-                });
-              });
-            });
-          } else {
-            venues.push({
-              "name":body.response.venue.name,
-              "city":body.response.venue.location.city,
-                  "location":{
-                    "lat":body.response.venue.location.lat, 
-                    "lng":body.response.venue.location.lng
-                  }
-            });
-          }
-          var response_body = JSON.stringify(venues);
-          var buf = new Buffer(response_body);
-          zlib.gzip(buf, function (_, result) {   
-            env.target.response.body = result; 
-            next(env);
-          });
-        }).on('error', function(error){
-          console.log(error);
-        })
-      } else {
-        env.target.response.getBody(function(error, body){
-          var venues = [];
-          var body = JSON.parse(body.toString('utf-8'));
-          if(body.response.groups) {
-            body.response.groups.forEach(function(group){
-              group.items.forEach(function(venue){
-                venues.push({
-                  "name":venue.venue.name, 
-                  "city":venue.venue.location.city,
-                  "location":{
-                    "lat":venue.venue.location.lat, 
-                    "lng":venue.venue.location.lng
-                  }
-                });
-              });
-            });
-          } else if(body.response.venue) {
-            venues.push({
-              "name":body.response.venue.name,
-              "city":body.response.venue.location.city,
-                  "location":{
-                    "lat":body.response.venue.location.lat, 
-                    "lng":body.response.venue.location.lng
-                  }
-            });
-          } else {
-            next(env);
-          }
-          env.target.response.body = JSON.stringify(venues); 
-          next(env);
-        });
-      }
+      env.target.response.getBody(function(err, body){
+         if(err) {
+           console.log(err);
+         } else {
+           console.log(body.toString());
+         }
+         next(env);
+      });
     });
   })
   .target('https://api.foursquare.com/v2/')
